be74118fb51121f23ceb5eaff12e05e9d3a4a76f
Carlos Bressan <105678823+Caduzinnn274@users.noreply.github.com>
2024-10-18 10:55:59 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Nome from './components/nome'
+
+/*function App() {
+  return (
+    <div className="App">
+      <h1>Testando o React</h1>
+    </div>
+  );
+}
+
+export default App;
+*/
+
+function App()
+{
+  return(
+    <div>
+      <h1>Nosso primeiro App React </h1> 
+      <h3>Usando Componete</h3>
+      <Nome pessoa = 'Carlos'></Nome> <br></br>
+    </div>
+  )
+}
+
+export default App;
+
---
+++ src/components/nome.js
@@ -0,0 +1,11 @@
+
+
+function Nome({pessoa})
+{
+  return(
+    <span>
+      Bem vindo {pessoa}
+    </span>
+  )
+}
+export default Nome;
